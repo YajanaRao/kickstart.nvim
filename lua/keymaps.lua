e6de5ef87d88c81fceacbaf8e60f5d48e64cf574
YajanaRao <yajananrao@gmail.com>
2024-12-06 20:37:22 +0530

    feat: minor improvements

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,35 +1,46 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-vim.keymap.set('n', ';', ':', { desc = 'CMD enter command mode' })
-
 -- Map `jk` and `jj` to escape in insert mode
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'jj', '<Esc>')
 
+-- Smart delete
+vim.keymap.set('n', 'dd', function()
+  if vim.api.nvim_get_current_line():match '^%s*$' then
+    return '"_dd'
+  else
+    return 'dd'
+  end
+end, { desc = "Smart dd, don't yank empty lines", expr = true })
+
 -- Buffers
 vim.keymap.set('n', '<leader>bd', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
--- vim.keymap.set('n', '<Leader>bn', ':bn<CR>', { desc = 'Next buffer' })
--- vim.keymap.set('n', '<Leader>bp', ':bp<CR>', { desc = 'Prev buffer' })
-vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Previous Buffer' })
+vim.keymap.set('n', '<C-l>', ':bn<CR>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<C-h>', ':bp<CR>', { desc = 'Prev buffer' })
 vim.keymap.set('n', '<TAB>', '<C-^>', { desc = 'Alternate buffers' })
-
--- Map move around with JK when in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+--  ─( Split "Sensibly" )───────────────────────────────────────────────
+local split_sensibly = function()
+  if vim.api.nvim_win_get_width(0) > math.floor(vim.api.nvim_win_get_height(0) * 2.3) then
+    vim.cmd 'vs'
+  else
+    vim.cmd 'split'
+  end
+end
+-- Should automatically split or vsplit based on Ratios
+vim.keymap.set('n', '<leader>bs', split_sensibly, { desc = 'Alternate buffers' })
 
 -- lines and characters movemnt for normal mode
-vim.keymap.set('n', '<M-j>', '7j')
-vim.keymap.set('n', '<M-k>', '7k')
-vim.keymap.set('n', '<M-l>', '25l')
-vim.keymap.set('n', '<M-h>', '25h')
+vim.keymap.set('n', 'J', '7j')
+vim.keymap.set('n', 'K', '7k')
+vim.keymap.set('n', 'L', '25l')
+vim.keymap.set('n', 'H', '25h')
 
 -- lines and characters movemnt for visual mode
-vim.keymap.set('v', '<M-j>', '7j')
-vim.keymap.set('v', '<M-k>', '7k')
-vim.keymap.set('v', '<M-l>', '25l')
-vim.keymap.set('v', '<M-h>', '25h')
+vim.keymap.set('v', 'J', '7j')
+vim.keymap.set('v', 'K', '7k')
+vim.keymap.set('v', 'L', '25l')
+vim.keymap.set('v', 'H', '25h')
 
 -- lines and characters movemnt for insert mode
 vim.keymap.set('i', '<M-Down>', '<C-o>7j')
@@ -37,13 +48,10 @@ vim.keymap.set('i', '<M-Up>', '<C-o>7k')
 vim.keymap.set('i', '<M-Right>', '<C-o>25l')
 vim.keymap.set('i', '<M-Left>', '<C-o>25h')
 
--- Move current line / block with Shift-j/k
-vim.keymap.set('n', 'J', ':m .+1<CR>==')
-vim.keymap.set('n', 'K', ':m .-2<CR>==')
-
 -- Write in normal mode
 vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
 
+-- TODO: figure out what does this do
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
@@ -63,15 +71,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -85,18 +84,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
-local split_sensibly = function()
-  if vim.api.nvim_win_get_width(0) > math.floor(vim.api.nvim_win_get_height(0) * 2.3) then
-    vim.cmd 'vs'
-  else
-    vim.cmd 'split'
-  end
-end
-
---  ─( Split "Sensibly" )───────────────────────────────────────────────
--- Should automatically split or vsplit based on Ratios
-vim.keymap.set('n', '<leader>bs', split_sensibly, { desc = 'Alternate buffers' })
 
 vim.keymap.set('n', '<leader>gg', function()
   vim.cmd 'terminal lazygit'
