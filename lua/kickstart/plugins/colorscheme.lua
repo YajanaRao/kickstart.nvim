e04c746c36019b455f2c4be1707f979e726d6a94
YajanaRao <yajananrao@gmail.com>
2025-02-01 23:37:00 +0530

    feat: everyforest

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -3,20 +3,30 @@ return {
   -- { 'rose-pine/neovim', name = 'rose-pine' },
   -- { 'ellisonleao/gruvbox.nvim', name = 'gruvbox' },
   {
-    -- 'catppuccin/nvim',
-    'folke/tokyonight.nvim',
-    name = 'tokyonight',
+    'YajanaRao/everforest',
     lazy = false,
     priority = 1000,
-    opts = {
-      -- flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      theme = 'tokyonight',
-      terminal_colors = true,
-      transparent = false,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.cmd.colorscheme 'everforest'
+    end,
   },
+  -- {
+  --   -- 'catppuccin/nvim',
+  --   'folke/tokyonight.nvim',
+  --   name = 'tokyonight',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     -- flavour = 'mocha', -- latte, frappe, macchiato, mocha
+  --     theme = 'tokyonight',
+  --     terminal_colors = true,
+  --     transparent = false,
+  --     styles = {
+  --       sidebars = 'transparent',
+  --       floats = 'transparent',
+  --     },
+  --   },
+  -- },
 }
