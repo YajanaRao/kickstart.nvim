3646982204b8028478f322d273c6955356c57a37
YajanaRao <yajananrao@gmail.com>
2025-05-19 20:10:54 +0530

    feat: window navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,10 +20,10 @@ vim.keymap.set('n', '<leader>bc', ':%bdelete<CR>', { desc = 'Close all buffers' 
 vim.keymap.set('n', '<TAB>', '<C-^>', { desc = 'Alternate buffers' })
 
 -- Move to window using the <ctrl> hjkl keys
-map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
-map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
-map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
-map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
 
 -- EXPERIMENT
 for i = 1, 9 do
