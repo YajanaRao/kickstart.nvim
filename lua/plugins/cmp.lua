54c4d31d7cdfc596688cc39303aac83ac34cb513
YajanaRao <yajananrao@gmail.com>
2025-08-29 22:56:51 +0530

    feat: snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,7 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-    },
+    dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
     event = 'InsertEnter',
     -- use a release tag to download pre-built binaries
     version = 'v0.*',
@@ -11,14 +9,13 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      snippets = { preset = 'luasnip' },
       keymap = { preset = 'default' },
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp
         -- will be removed in a future release
         use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
 
@@ -44,8 +41,11 @@ return {
       },
       completion = {
         accept = { auto_brackets = { enabled = true } },
-        menu = { border = vim.g.borderStyle },
         documentation = { window = { border = vim.g.borderStyle } },
+        menu = {
+          border = vim.g.borderStyle,
+        },
+        ghost_text = { enabled = true },
       },
       signature = { enabled = true, window = { border = vim.g.borderStyle } },
     },
